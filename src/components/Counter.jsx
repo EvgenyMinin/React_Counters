9a62d23f5053f973879d048f946ba62cfd8dc3a9
JOHNNY\Евгений <zhenya.minin.e-mail@rambler.ru>
2019-02-10 21:07:23 +0300

    обработка событий

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        tags: []
+        tags: ['tag1', 'tag2', 'tag3']
     };
 
     renderTags() {
@@ -12,9 +12,32 @@ class Counter extends Component {
         return <ul>{ this.state.tags.map(tag => <li key={tag}>{tag}</li>) }</ul>
     }
 
+    getBadgeClasses() {
+        let classes = 'badge  m-2 badge-';
+        classes += this.state.count === 0 ? 'warning' : 'primary';
+
+        return classes;
+    }
+
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+
+    handleIncrement() {
+        console.log('Increment clicked');
+    }
+
     render() {        
         return (
             <React.Fragment>
+                <span className={this.getBadgeClasses()}>{ this.formatCount() }</span>
+                <button
+                    className='btn btn-secondary'
+                    onClick={this.handleIncrement}
+                >
+                    Increment
+                </button>
                 { this.state.tags.length === 0 && 'Please create a new tag!' }
                 { this.renderTags() }
             </React.Fragment>
