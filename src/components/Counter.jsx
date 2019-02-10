f0fa45e35077363ab0edddf889dcf0b687e855a8
JOHNNY\Евгений <zhenya.minin.e-mail@rambler.ru>
2019-02-10 18:04:43 +0300

    динамический рендеринг класса

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -10,22 +10,22 @@ class Counter extends Component {
         return count === 0 ? 'Zero' : count;
     }
 
-    render() { 
+    getBadgeClasses() {
+        let classes = 'badge m-2 badge-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
+    }
+
+    render() {        
         return (
             <React.Fragment>
-                <span
-                    className='badge badge-primary m-2'
-                >
-                    { this.formatCount() }
-                </span>
-                <button
-                    className='btn btn-secondary'
-                >
-                    Increment
-                </button>
+                <span className={this.getBadgeClasses()}>{ this.formatCount() }</span>
+                <button className='btn btn-secondary'>Increment</button>
             </React.Fragment>
         );
     }
+
+    
 }
  
 export default Counter;
