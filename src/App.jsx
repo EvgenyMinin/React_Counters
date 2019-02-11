05c1cc5a5ad864ee1c61b466211776ae8d71f6d2
JOHNNY\Евгений <zhenya.minin.e-mail@rambler.ru>
2019-02-11 21:01:54 +0300

    Lifting the State up

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,52 @@
 import React, { Component } from 'react';
 import Counters from './components/Counters';
+import NavBar from './components/NavBar';
 
 class App extends Component {
+
+    state = {
+        counters: [
+            { id: 1, value: 4 },
+            { id: 2, value: 0 },
+            { id: 3, value: 0 },
+            { id: 4, value: 0 },
+        ]
+    }
+
+    handleDelete = counterId => {
+        const counters = this.state.counters.filter(c => c.id !== counterId);
+        this.setState({ counters })
+    }
+
+    handleReset = () => {
+        const counters = this.state.counters.map(c => {
+            c.value = 0;
+            return c;
+        });
+        this.setState({ counters })
+    }
+
+    handleIncrement = counter => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+        this.setState({ counters })
+    }
+
     render() {
         return (
-            <Counters />
+            <React.Fragment>
+                <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length} />
+                <main className="container">
+                    <Counters
+                        counters={this.state.counters} 
+                        onReset={this.handleReset}
+                        onDelete={this.handleDelete}
+                        onIncrement={this.handleIncrement}
+                    />
+                </main>
+            </React.Fragment>
         );
     }
 }
